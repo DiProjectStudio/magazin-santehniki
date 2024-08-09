5f5307a73f0469898dfda8e1eb4a7b6cbd287344
IlyaDOC <ilya_stolyar@inbox.ru>
2024-08-09 12:02:38 +0300

    add footer styles

--- src/assets/js/navbar.js
+++ src/assets/js/navbar.js
@@ -1,7 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    navbar();
+})
+
+
 function navbar() {
     const burgerToggle = document.getElementById('burgerToggle');
-    burgerToggle.addEventListener('click', (event)=> {
+    const navMenu = document.querySelector('.header__inner_menu');
+    const closeBtn = document.getElementById('closeBtn');
 
+    burgerToggle.addEventListener('click', (event) => {
+        navMenu.classList.toggle('active');
         event.stopPropagation();
+    });
+
+    document.addEventListener('click', function (event) {
+        if (!event.composedPath().includes(navMenu)) {
+            navMenu.classList.remove('active');
+
+        }
+    });
+
+    closeBtn.addEventListener('click', () => {
+        navMenu.classList.remove('active');
     });
 }

544d2b19b2ec91733e628b9c724411dd97b7c23d
IlyaDOC <ilya_stolyar@inbox.ru>
2024-08-09 12:32:04 +0300

    add overlay

--- src/assets/js/navbar.js
+++ src/assets/js/navbar.js
@@ -10,17 +10,29 @@ function navbar() {
 
     burgerToggle.addEventListener('click', (event) => {
         navMenu.classList.toggle('active');
+        toggleOverlay(true);
         event.stopPropagation();
     });
 
     document.addEventListener('click', function (event) {
         if (!event.composedPath().includes(navMenu)) {
             navMenu.classList.remove('active');
+            toggleOverlay(false);
 
         }
     });
 
     closeBtn.addEventListener('click', () => {
         navMenu.classList.remove('active');
+        toggleOverlay(false);
     });
+}
+
+function toggleOverlay(condition) {
+    const overlay = document.querySelector('.header__inner_overlay');
+    if (condition === true) {
+        overlay.style.display = 'block';
+    } else if (condition === false) {
+        overlay.style.display = 'none';
+    }
 }

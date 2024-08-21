1a708642594e81e92f9d3b8b2471aad537b67189
IlyaDOC <ilya_stolyar@inbox.ru>
2024-08-21 17:24:58 +0300

    add lg version and fix problem with position sticky

--- src/assets/js/product.js
+++ src/assets/js/product.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    tabsAction();
-})
+    tabsAction()
+    moveElements();
+});
+
+document.addEventListener('resize', () => {
+    moveElements();
+});
 
 
 function tabsAction() {
@@ -12,7 +17,7 @@ function tabsAction() {
             const tabAttribute = tabButton.getAttribute('data-tab');
 
             // Удаляем класс active у всех кнопок и панелей
-            tabButtons.forEach( btn => btn.classList.remove('active'));
+            tabButtons.forEach(btn => btn.classList.remove('active'));
             tabPanes.forEach(pane => pane.classList.remove('active'));
 
             // Добавляем класс active к текущей кнопке и соответствующей панели
@@ -20,4 +25,23 @@ function tabsAction() {
             document.getElementById(tabAttribute).classList.add('active');
         });
     });
+}
+
+
+function moveElements() {
+    const productTitle = document.querySelector('.product .product__title');
+    const productSpecifications = document.querySelector('.product .product__specifications');
+    const destinationBlockRight = document.querySelector('.product__js-right');
+
+    const productImages = document.querySelector('.product .product__images');
+    const productAbout = document.querySelector('.product .product__about');
+    const destinationBlockLeft = document.querySelector('.product__js-left');
+
+
+    if ((productTitle && productSpecifications && productImages && productAbout) && window.innerWidth >= 1200) {
+        destinationBlockRight.appendChild(productTitle);
+        destinationBlockRight.appendChild(productSpecifications);
+        destinationBlockLeft.appendChild(productImages);
+        destinationBlockLeft.appendChild(productAbout);
+    }
 }
